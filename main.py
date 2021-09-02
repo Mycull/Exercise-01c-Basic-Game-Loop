48bb8a8c628f65036f69ba9c00deee27f0561e34
Mycull <82784090+Mycull@users.noreply.github.com>
2021-09-02 00:55:56 -0400

    Update main.py
    # Exercise-01c-Basic-Game-Loop
    Exercise for MSCH-C220, 31 August 2021
    
    A simple version of a game loop, designed to navigate an interactive fiction world (contained in a dictionary), implemented in Python.
    
    ## Implementation
    Created using Python 3.9
    
    ## References
    [Zork, 1977 by Tim Anderson, Marc Blank, Dave Lebling, and Bruce Daniels](https://en.wikipedia.org/wiki/Zork)
    
    ## Future Development
    None
    
    ## Created by
    Michael Truong

--- main.py
+++ main.py
@@ -52,3 +52,21 @@ while True:
       if response == link["linkText"]:
         current = link["passageName"]
   
+"""
+# Exercise-01c-Basic-Game-Loop
+Exercise for MSCH-C220, 31 August 2021
+
+A simple version of a game loop, designed to navigate an interactive fiction world (contained in a dictionary), implemented in Python.
+
+## Implementation
+Created using Python 3.9
+
+## References
+[Zork, 1977 by Tim Anderson, Marc Blank, Dave Lebling, and Bruce Daniels](https://en.wikipedia.org/wiki/Zork)
+
+## Future Development
+None
+
+## Created by
+Michael Truong
+"""
